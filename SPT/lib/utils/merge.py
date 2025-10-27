fb2f5ceaba47fedb9523d40be07b896db4650bc8
mx-pai <xuma5668@gmail.com>
2025-10-27 16:49:07 +0800

    Remove unused get_qkv helper

--- SPT/lib/utils/merge.py
+++ SPT/lib/utils/merge.py
@@ -13,17 +13,3 @@ def merge_template_search(inp_list, return_search=False, return_template=False):
         z = inp_list[0]
         seq_dict.update({"feat_z": z["feat"], "mask_z": z["mask"], "pos_z": z["pos"]})
     return seq_dict
-
-
-def get_qkv(inp_list):
-    """The 1st element of the inp_list is about the template,
-    the 2nd (the last) element is about the search region"""
-    dict_x = inp_list[-1]
-    dict_c = {"feat": torch.cat([x["feat"] for x in inp_list], dim=0),
-              "mask": torch.cat([x["mask"] for x in inp_list], dim=1),
-              "pos": torch.cat([x["pos"] for x in inp_list], dim=0)}  # concatenated dict
-    q = dict_x["feat"] + dict_x["pos"]
-    k = dict_c["feat"] + dict_c["pos"]
-    v = dict_c["feat"]
-    key_padding_mask = dict_c["mask"]
-    return q, k, v, key_padding_mask
