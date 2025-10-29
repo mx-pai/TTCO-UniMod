f3498949f194718c92fb8b34181cb23c6dc1c7ff
mx-pai <xuma5668@gmail.com>
2025-10-29 20:55:32 +0800

    fix: reintroduce default local generator for cli script

--- SPT/lib/train/admin/environment.py
+++ SPT/lib/train/admin/environment.py
@@ -1,19 +1,17 @@
 import importlib
-
-
-class EnvSettings:
-    """Base environment settings. Values are intended to be overridden in local.py."""
-
-    def __init__(self):
-        self.workspace_dir = ''
-        self.tensorboard_dir = ''
-        self.pretrained_models = ''
-        self.unimod1k_dir = ''
-        self.unimod1k_dir_nlp = ''
+import os
 
 
 def env_settings():
-    """Load user-defined EnvironmentSettings from lib.train.admin.local."""
     env_module = importlib.import_module('lib.train.admin.local')
     env_cls = getattr(env_module, 'EnvironmentSettings')
     return env_cls()
+
+
+def create_default_local_file_ITP_train(workspace_dir, data_dir):
+    path = os.path.join(os.path.dirname(__file__), 'local.py')
+    tensorboard_dir = os.path.join(workspace_dir, 'tensorboard')
+    pretrained_dir = os.path.join(workspace_dir, 'pretrained_models')
+    content = f"""class EnvironmentSettings:\n    def __init__(self):\n        self.workspace_dir = '{workspace_dir}'\n        self.tensorboard_dir = '{tensorboard_dir}'\n        self.pretrained_models = '{pretrained_dir}'\n        self.unimod1k_dir = '{data_dir}'\n        self.unimod1k_dir_nlp = '{data_dir}'\n"""
+    with open(path, 'w') as f:
+        f.write(content)
